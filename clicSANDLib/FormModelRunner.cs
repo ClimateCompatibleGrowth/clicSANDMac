e6a9936572a5454d71a769de4f39cc173f92cc2f
alexanderkell <alexander@kell.es>
2022-02-04 17:10:55 +0000

    working on adding python to c#

--- clicSANDLib/FormModelRunner.cs
+++ clicSANDLib/FormModelRunner.cs
@@ -47,6 +47,14 @@ namespace clicSANDLib
             {
                 MessageBox.Show(exc.Message, "Error Running Model");
             }
+            try
+            {
+                ConvertResults(resultsFileName, resultsFileName);
+            }
+            catch (Exception exc)
+            {
+                textBoxOutput.Text += "Unable to convert results: " + exc.Message + "\r\n";
+            }
             finally
             {
                 //TODO Cursor.Current = Cursors.Default;
@@ -62,160 +70,186 @@ namespace clicSANDLib
             }
         }
 
-        //private bool ExtractDataFromXLS(string xlsFileName, string dataFileName)
-        //{
-        //    try
-        //    {
-        //        var Excel = new Excel.Application();
-        //        var workBook = Excel.Workbooks.Open(xlsFileName);
-        //        Excel.Application.Visible = true;
-        //        Excel.Application.Run(String.Format("'{0}'!Module1.writefile", xlsFileName));
-        //        workBook.Saved = true;
-        //        workBook.Close();
-        //        Excel.Quit();
-        //    }
-        //    catch (Exception exc)
-        //    {
-        //        textBoxOutput.Text += String.Format("Error extracting data:\r\n{0}", exc.Message);
-        //        return false;
-        //    }
-        //    return true;
-        //}
-        private bool RunGLPSOL(string dataFileName, string modelFileName, string lpFileName)
+
+    }
+    //private bool ExtractDataFromXLS(string xlsFileName, string dataFileName)
+    //{
+    //    try
+    //    {
+    //        var Excel = new Excel.Application();
+    //        var workBook = Excel.Workbooks.Open(xlsFileName);
+    //        Excel.Application.Visible = true;
+    //        Excel.Application.Run(String.Format("'{0}'!Module1.writefile", xlsFileName));
+    //        workBook.Saved = true;
+    //        workBook.Close();
+    //        Excel.Quit();
+    //    }
+    //    catch (Exception exc)
+    //    {
+    //        textBoxOutput.Text += String.Format("Error extracting data:\r\n{0}", exc.Message);
+    //        return false;
+    //    }
+    //    return true;
+    //}
+
+    static void ConvertResults(string input, string output_dir)
+    {
+        // Use ProcessStartInfo class
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.CreateNoWindow = false;
+        startInfo.UseShellExecute = false;
+        startInfo.FileName = "/Users/alexanderkell/GitHub/SGI/1-cliSANDMac/converter/dist/python_converter";
+        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        startInfo.Arguments = "-f j -o \"" + input + "\" -z 1.0 -s y " + output_dir;
+
+        try
         {
-            return RunProcess(String.Format(@"{0}/glpsol", ConfigurationManager.AppSettings["glpsolLocation"]), string.Format("--check -m \"{0}\" -d \"{1}\" --wlp \"{2}\"", modelFileName, dataFileName, lpFileName));
+            using (Process exeProcess = Process.Start(startInfo))
+            {
+                extBoxOutput.Text += "Running python converter";
+                exeProcess.WaitForExit();
+            }
         }
-
-        private bool RunCBC(string inputFileName, string outputFileName, bool applyRatio, string ratio)
+        catch (Exception exc)
         {
-            string cbcParams = null;
-
-            if (applyRatio)
-            {
-                cbcParams = String.Format("\"{0}\" ratio {1} solve -solu \"{2}\"", inputFileName, ratio, outputFileName);
-            }
-            else
-            {
-                cbcParams = String.Format("\"{0}\" solve -solu \"{1}\"", inputFileName, outputFileName);
-            }
-            return RunProcess(String.Format(@"{0}/cbc", ConfigurationManager.AppSettings["cbcLocation"]), cbcParams);
+            textBoxOutput.Text += "Unable to run python converter: " + exc.Message + "\r\n";
         }
+    }
+    private bool RunGLPSOL(string dataFileName, string modelFileName, string lpFileName)
+    {
+        return RunProcess(String.Format(@"{0}/glpsol", ConfigurationManager.AppSettings["glpsolLocation"]), string.Format("--check -m \"{0}\" -d \"{1}\" --wlp \"{2}\"", modelFileName, dataFileName, lpFileName));
+    }
 
-        private bool RunProcess(string filename, string args)
+    private bool RunCBC(string inputFileName, string outputFileName, bool applyRatio, string ratio)
+    {
+        string cbcParams = null;
+
+        if (applyRatio)
+        {
+            cbcParams = String.Format("\"{0}\" ratio {1} solve -solu \"{2}\"", inputFileName, ratio, outputFileName);
+        }
+        else
+        {
+            cbcParams = String.Format("\"{0}\" solve -solu \"{1}\"", inputFileName, outputFileName);
+        }
+        return RunProcess(String.Format(@"{0}/cbc", ConfigurationManager.AppSettings["cbcLocation"]), cbcParams);
+    }
+
+    private bool RunProcess(string filename, string args)
+    {
+        try
+        {
+            textBoxOutput.Text += new string('-', 150) + "\r\n";
+            textBoxOutput.Text += string.Format("Running {0} {1}\r\n", filename, args);
+            textBoxOutput.Text += new string('-', 150) + "\r\n";
+            Process compiler = new Process();
+            compiler.StartInfo.FileName = filename;
+            compiler.StartInfo.Arguments = args;
+            compiler.StartInfo.UseShellExecute = false;
+            compiler.StartInfo.RedirectStandardOutput = true;
+            compiler.StartInfo.CreateNoWindow = false;
+
+            compiler.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                // Prepend line numbers to each line of the output.
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    textBoxOutput.Text += e.Data;
+                }
+            });
+
+            compiler.Start();
+
+            textBoxOutput.Text += compiler.StandardOutput.ReadToEnd();
+
+            compiler.WaitForExit();
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            textBoxOutput.Text += "Error: " + e.Message + "\r\n";
+            return false;
+        }
+    }
+
+    private void buttonOpenDataSource_Click(object sender, EventArgs e)
+    {
+        var filename = OpenFile("Model input files|*.txt");
+
+        if (!String.IsNullOrEmpty(filename))
+        {
+            textBoxDataSource.Text = filename;
+        }
+    }
+
+    private string OpenFile(string filter)
+    {
+        var fileDialog = new OpenFileDialog();
+        string[] filterparts = filter.Split(new char[] { '|' });
+        fileDialog.Filters.Add(new FileFilter(filterparts[0], new string[] { filterparts[1] }));
+        //fileDialog.Multiselect = false;
+
+        DialogResult dialogResult = fileDialog.ShowDialog(this);
+
+        if (dialogResult.Equals(DialogResult.Ok))
+        {
+            return fileDialog.FileName;
+        }
+        return null;
+    }
+
+    protected void buttonOpenModel_Click(object sender, EventArgs e)
+    {
+        var filename = OpenFile("Text files|*.txt");
+
+        if (!String.IsNullOrEmpty(filename))
+        {
+            textBoxModel.Text = filename;
+        }
+    }
+
+    private void SaveLog(string logFileName, string lines)
+    {
+        System.IO.StreamWriter file = new StreamWriter(logFileName);
+        file.WriteLine(lines);
+    }
+
+    protected void buttonOpenResults_Click(object sender, EventArgs e)
+    {
+        RunProcess(@"/usr/bin/open", " -a TextEdit " + logFileName);
+    }
+
+    private void checkCBCRatio_CheckedChanged(object sender, EventArgs e)
+    {
+        numericRatio.Enabled = (bool)checkCBCRatio.Checked;
+    }
+
+    protected void buttonSaveTemplates_Click(object sender, EventArgs e)
+    {
+        SelectFolderDialog folderDialog = new SelectFolderDialog();
+        folderDialog.Title = "Extract templates";
+        //folderDialog.ShowNewFolderButton = true;
+        DialogResult result = folderDialog.ShowDialog(this);
+
+        var location = new Uri(System.Reflection.Assembly.GetEntryAssembly().GetName().CodeBase);
+        DirectoryInfo runningFolder = new FileInfo(location.AbsolutePath).Directory;
+
+        if (result.Equals(DialogResult.Ok))
         {
             try
             {
-                textBoxOutput.Text += new string('-', 150) + "\r\n";
-                textBoxOutput.Text += string.Format("Running {0} {1}\r\n", filename, args);
-                textBoxOutput.Text += new string('-', 150) + "\r\n";
-                Process compiler = new Process();
-                compiler.StartInfo.FileName = filename;
-                compiler.StartInfo.Arguments = args;
-                compiler.StartInfo.UseShellExecute = false;
-                compiler.StartInfo.RedirectStandardOutput = true;
-                compiler.StartInfo.CreateNoWindow = false;
-
-                compiler.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+                foreach (FileInfo file in runningFolder.GetDirectories("Templates")[0].GetFiles())
                 {
-                    // Prepend line numbers to each line of the output.
-                    if (!String.IsNullOrEmpty(e.Data))
-                    {
-                        textBoxOutput.Text += e.Data;
-                    }
-                });
-
-                compiler.Start();
-
-                textBoxOutput.Text += compiler.StandardOutput.ReadToEnd();
-
-                compiler.WaitForExit();
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                textBoxOutput.Text += "Error: " + e.Message + "\r\n";
-                return false;
-            }
-        }
-
-        private void buttonOpenDataSource_Click(object sender, EventArgs e)
-        {
-            var filename = OpenFile("Model input files|*.txt");
-
-            if (!String.IsNullOrEmpty(filename))
-            {
-                textBoxDataSource.Text = filename;
-            }
-        }
-
-        private string OpenFile(string filter)
-        {
-            var fileDialog = new OpenFileDialog();
-            string[] filterparts = filter.Split(new char[] { '|' });
-            fileDialog.Filters.Add(new FileFilter(filterparts[0], new string[] { filterparts[1] }));
-            //fileDialog.Multiselect = false;
-
-            DialogResult dialogResult = fileDialog.ShowDialog(this);
-
-            if (dialogResult.Equals(DialogResult.Ok))
-            {
-                return fileDialog.FileName;
-            }
-            return null;
-        }
-
-        protected void buttonOpenModel_Click(object sender, EventArgs e)
-        {
-            var filename = OpenFile("Text files|*.txt");
-
-            if (!String.IsNullOrEmpty(filename))
-            {
-                textBoxModel.Text = filename;
-            }
-        }
-
-        private void SaveLog(string logFileName, string lines)
-        {
-            System.IO.StreamWriter file = new StreamWriter(logFileName);
-            file.WriteLine(lines);
-        }
-
-        protected void buttonOpenResults_Click(object sender, EventArgs e)
-        {
-            RunProcess(@"/usr/bin/open", " -a TextEdit " + logFileName);
-        }
-
-        private void checkCBCRatio_CheckedChanged(object sender, EventArgs e)
-        {
-            numericRatio.Enabled = (bool)checkCBCRatio.Checked;
-        }
-
-        protected void buttonSaveTemplates_Click(object sender, EventArgs e)
-        {
-            SelectFolderDialog folderDialog = new SelectFolderDialog();
-            folderDialog.Title = "Extract templates";
-            //folderDialog.ShowNewFolderButton = true;
-            DialogResult result = folderDialog.ShowDialog(this);
-
-            var location = new Uri(System.Reflection.Assembly.GetEntryAssembly().GetName().CodeBase);
-            DirectoryInfo runningFolder = new FileInfo(location.AbsolutePath).Directory;
-
-            if (result.Equals(DialogResult.Ok))
-            {
-                try
-                {
-                    foreach (FileInfo file in runningFolder.GetDirectories("Templates")[0].GetFiles())
-                    {
-                        File.Copy(file.FullName, Path.Combine(folderDialog.Directory, file.Name), true);
-                    }
-                    MessageBox.Show(String.Format("Templates exported to {0}", folderDialog.Directory));
+                    File.Copy(file.FullName, Path.Combine(folderDialog.Directory, file.Name), true);
                 }
-                catch (Exception exc)
-                {
-                    MessageBox.Show(String.Format("Failed to copy files: \r\n{0}", exc.Message));
-                }
+                MessageBox.Show(String.Format("Templates exported to {0}", folderDialog.Directory));
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(String.Format("Failed to copy files: \r\n{0}", exc.Message));
             }
         }
     }
 }
+
